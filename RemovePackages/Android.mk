57eb27ac57546ef4cdb0cae36eba3abebae316b4
H3M3L <lllooolllzzz555@gmail.com>
2021-09-11 08:55:59 +0000

    laurel_sprout: nuked some packages while building

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,11 +4,12 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Camera2 CarrierSetup
-LOCAL_OVERRIDES_PACKAGES += MyVerizonServices Music
-LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions OPScreenRecord RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SprintDM SprintHM
-LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AndroidAutoStubPrebuilt AppDirectedSMSService Camera2
+LOCAL_OVERRIDES_PACKAGES += ConnMO DCMO DMService GCS GoogleCamera MaestroPrebuilt Maps
+LOCAL_OVERRIDES_PACKAGES += MicropaperPrebuilt MyVerizonServices OBDM_Permissions OemDmTrigger Ornament
+LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap SoundAmplifierPrebuilt
+LOCAL_OVERRIDES_PACKAGES += SprintDM SprintHM Tycho USCCDM VZWAPNLib VzwOmaTrigger WallpapersBReel2020
+LOCAL_OVERRIDES_PACKAGES += Youtube YouTubeMusicPrebuilt arcore obdm_stub
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
